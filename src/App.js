79f14795494609f7c1e41be6c96243c0078362b8
unknown <sdak006@gmail.com>
2017-06-11 11:20:11 -0500

    Added log out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginFrom from './components/LoginForm';
 
 class App extends Component {
+    state = {
+        loggedIn: null
+    };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyD_2BYaX4_QKUK7nL6hcTXYmR5yNXk5rsI',
@@ -14,13 +18,44 @@ class App extends Component {
             storageBucket: 'auth-a7396.appspot.com',
             messagingSenderId: "54138554713"
         });
+
+        firebase.auth().onAuthStateChanged((user)=> {
+            if (user) {
+                console.log('logged in');
+                this.setState({ loggedIn: true });
+            } else {
+                console.log('logged out');
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>
+                );
+            case false:
+                return <LoginFrom />;
+            default:
+                return (
+                    <CardSection>
+                        <Spinner size='large' />
+                    </CardSection>
+                );
+        }
     }
 
     render () {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginFrom />
+                {this.renderContent()}
             </View>
         );
     }
