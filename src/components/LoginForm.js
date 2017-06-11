4ebf2c47f364830e54795858ec2ae68ad0636363
unknown <sdak006@gmail.com>
2017-06-10 20:38:30 -0500

    Input abstraction

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { text: '' };
+    state = { 
+        email: '',
+        password: ''
+    };
 
     render() {
         return (
             <Card>
                 <CardSection>
-                    <TextInput
-                        value={this.state.text}
-                        onChangeText={text => this.setState({ text })}
-                        style={{padding: 5, width: 100, flex: 1}}>
-                    </TextInput>
+                    <Input
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                        labelText='Email'
+                        placeholder='user@gmail.com'
+                    />
                 </CardSection>
                     
                 <CardSection>
+                    <Input
+                        secureTextEntry
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                        labelText='Password'
+                        placeholder='password'
+                    />
                 </CardSection>
 
                 <CardSection>
